0ae609fd60d4d7593bed143b5e6da71c4bc8693c
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-10-02 11:52:46 +0200

    second commit

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ var http = require("http");
 var fs = require("fs");
 
 var server = http.createServer(function(request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === "GET" && request.url === "/") {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
         response.write("<h1>Hello World!<h1>");
         response.end();
-    } else {
-        response.statusCode = 404;
+    } else if (response.statusCode = 404) {
+        response.setHeader("Content-Type", "image/jpeg");
         fs.readFile("./monster.jpg", function(err, data) {
             if (err) throw err;
-            console.log(data);
-        })
+            response.write(data);
+            response.end(); 
+        });     
     }
 });
 
